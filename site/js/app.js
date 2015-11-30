a177beb0a368584903a8f3df35f86e9334e3f2bb
valeraderevshukov <kaktuz34@gmail.com>
2015-11-30 18:33:32 +0200

    index

--- site/js/app.js
+++ site/js/app.js
@@ -32,12 +32,13 @@ $(document).ready(function() {
 			slidesToShow: 1,
 			slidesToScroll: 1,
 			speed: 700,
+			fade: true,
 			autoplay: true,
 			autoplaySpeed: 2000,
 			appendArrows: arr,
 			appendDots: arr,
-			prevArrow: '<a href="#" class="arrow is-prev"><i class="icon icon-uni57"></i></a>',
-			nextArrow: '<a href="#" class="arrow is-next"><i class="icon icon-uni77"></i></a>',
+			prevArrow: '<a href="#" class="arrow is-prev"><i class="icon icon-uni77"></i></a>',
+			nextArrow: '<a href="#" class="arrow is-next"><i class="icon icon-uni57"></i></a>',
 			responsive: [
 				{
 					breakpoint: 1024,
