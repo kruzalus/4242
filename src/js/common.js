06cf6f6766edc50c382cd6d9c6e600889cfaec3d
valeraderevshukov <kaktuz34@gmail.com>
2015-12-09 15:05:11 +0200

    resp index

--- src/js/common.js
+++ src/js/common.js
@@ -5,22 +5,22 @@ $(document).ready(function() {
 		var slider 	= $(this),
 			arr 	= slider.parent().find('.arrows');
 		slider.slick({
-			dots: true,
-			infinite: true,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			speed: 700,
-			fade: true,
-			autoplay: true,
-			autoplaySpeed: 2000,
-			appendArrows: arr,
-			appendDots: arr,
-			prevArrow: '<a href="#" class="arrow is-prev"><i class="icon icon-uni77"></i></a>',
-			nextArrow: '<a href="#" class="arrow is-next"><i class="icon icon-uni57"></i></a>',
-			responsive: [
+			dots 			: true,
+			infinite 		: true,
+			slidesToShow 	: 1,
+			slidesToScroll 	: 1,
+			speed 			: 700,
+			fade 			: true,
+			autoplay 		: true,
+			autoplaySpeed 	: 2000,
+			appendArrows 	: arr,
+			appendDots 		: arr,
+			prevArrow 		: '<a href="#" class="arrow is-prev"><i class="icon icon-uni77"></i></a>',
+			nextArrow 		: '<a href="#" class="arrow is-next"><i class="icon icon-uni57"></i></a>',
+			responsive 		: [
 				{
-					breakpoint: 1024,
-					settings: {
+					breakpoint 	: 1024,
+					settings 	: {
 						adaptiveHeight: true
 					}
 				}
@@ -34,9 +34,15 @@ $(document).ready(function() {
 		$(this).parents('.js-select').find('.js-input').val(text);
 	});
 
+	// open menu
 	$('.js-btn-header').on('click', function(){
+		var wind 	= $(window).height(),
+			out 	= $('.out').height();
+		if(out > wind) {
+			$('body').toggleClass('is-scroll');
+		}
 		$(this).toggleClass('is-active');
-		$('.js-header-block').toggleClass('is-open');
+		$('body').toggleClass('is-open-menu');
 	});
 
 
