acd7ca0c8fe3f18565cefed4a915f2ca83e75c86
valeraderevshukov <kaktuz34@gmail.com>
2015-12-24 11:20:46 +0200

    upd

--- site/js/common.js
+++ site/js/common.js
@@ -229,18 +229,25 @@ $(document).ready(function() {
 				arrErr 	= loader.find('.js-arr-error');
 			if ($('.js-preloader').hasClass('is-valid')) {
 				arrVal.show();
+
 				var svg = arrVal.drawsvg({
 					reverse: true
 				});
 				svg.drawsvg('animate');
+
+				$('.js-loader-load').hide();
+				$('.js-loader-valid').fadeIn(1000);
 			}
 			else if ($('.js-preloader').hasClass('is-error')) {
 				arrErr.show();
+
 				var svg = arrErr.drawsvg({
-					stagger: 500,
-					duration: 800
+					stagger: 300,
+					duration: 500
 				});
 				svg.drawsvg('animate');
+				$('.js-loader-load').hide();
+				$('.js-loader-error').fadeIn(1000);
 			}
 		}, 100);
 	} animateLoad();
