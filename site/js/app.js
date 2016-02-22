d7ea38bda6e5e6d543dcb45be2d2569721ebe130
valeraderevshukov <kaktuz34@gmail.com>
2016-02-22 15:29:34 +0200

    add graph and popup, page current_balance_of_gold_coins.html

--- site/js/app.js
+++ site/js/app.js
@@ -4378,87 +4378,86 @@ $(document).ready(function() {
 	$(function () {
 	    var seriesOptions = [],
 	        seriesCounter = 0,
-	        names = ['MSFT', 'AAPL', 'GOOG'];
+	        names = ['MSFT', 'AAPL'];
 
 	    function createChart() {
 
-	    	// Highcharts.setOptions({
-		    //     // lang: {
-		    //     //     resetZoom:'Zoom zurücksetzen',
-		    //     //     decimalPoint: ',',
-		    //     //     thousandsSep: '.'
-		    //     // }
-		    // });
 			$('.js-graph').highcharts('StockChart', {
 				
 				rangeSelector: {
-					selected: 4,
-					buttons: [{
-						type: 'month',
-						count: 1,
-						text: 'Месяц'},
+					selected: 2,
+					buttons: [
 					{
-						type: 'month',
-						count: 3,
-						text: 'Квартал'},
+						type: 'all',
+						text: 'Все время'},
 					{
 						type: 'year',
 						count: 1,
 						text: 'Год'},
 					{
-						type: 'all',
-						text: 'Все время'}],
+						type: 'month',
+						count: 3,
+						text: 'Квартал'},
+					{
+						type: 'month',
+						count: 1,
+						text: 'Месяц'
+					}],
 					buttonTheme: {
-						width: 60,
-						fill: 'none',
-						stroke: 'none',
+						width: 80,
+						height: 8,
+						fill: '#fff',
+						'stroke-width': 1,
+						'stroke': '#ECEFF1',
+						r: 3,
 						style: {
-							color: 'red',
-							fontWeight: '400',
-							fontSize:'.8em'
+							color: '#90A4AE',
+							fontWeight: 'normal',
+							fontFamily: '"Roboto Light", Arial, sans-serif',
+							fontSize: '10px'
 						},
 						states: {
 							hover: {
-							    fill: 'white'
+							    fill: '#ECEFF1',
+								'stroke-width': 0
 							},
 							select: {
+								'stroke-width': 0,
+								fill: '#ECEFF1',
 								style: {
-									color: 'blue'
+									color: '#90A4AE',
+									fontWeight: 'normal'
 								}
 							}
 						},
-						width: null,
-						padding: 2
+						width: 50,
+						padding: 7,
+						lineHeight: 1
 					}
 				},
 
 				chart: {
-					height: 500
+					height: 500,
+					marginTop: -34,
+					marginBottom: 58
 				},
 
 				xAxis: {
 					gridLineWidth: 1,
 					lineColor: '#ECEFF1',
-					tickColor: '#ECEFF1',
+					gridLineColor: '#ECEFF1',
+					tickWidth: 0,
 					labels: {
 						style: {
-							color: '#000',
-							font: '13px Trebuchet MS, Verdana, sans-serif'
+							color: '#90A4AE',
+							font: '10px "Roboto Light", Arial, sans-serif'
 						}
 
-					},
-					title: {
-						style: {
-							color: '#333',
-							fontWeight: 'bold',
-							fontSize: '12px',
-							fontFamily: 'Trebuchet MS, Verdana, sans-serif'
-
-						}
 					}
 				},
 
 				yAxis: {
+					gridLineColor: '#ECEFF1',
 					labels: {
 						enabled: false
 					}
@@ -4469,7 +4468,21 @@ $(document).ready(function() {
 						compare: 'percent',
 						animation: {
 							duration: 2000
-						}
+						},
+						marker: {
+							symbol: 'circle',
+							lineWidth: 0,
+							radius: 1.2,
+							lineColor: null
+						},
+						states: {
+							hover: {
+								halo: {
+									size: 0
+								}
+							}
+						},
+						lineWidth: 1
 					}
 				},
 
@@ -4478,11 +4491,16 @@ $(document).ready(function() {
 				},
 
 				tooltip: {
-					pointFormat: '<span>{series.name}</span>: <br/><b style="color:{series.color}">{point.y}</b><br/>',
+					pointFormat: '<span style="color:#90A4AE; font-size: 10px;" >{series.name}</span>: <br/><b style="color:{series.color}; font-size: 10px;">{point.y}</b><br/>',
 					valueDecimals: 2,
+					fontFamily: '"Roboto Light", Arial, sans-serif',
 					borderRadius: 0,
 					shadow: false,
-					borderColor: '#eceff1'
+					borderColor: '#eceff1',
+					backgroundColor: 'rgba(255, 255, 255, 0.9)',
+					crosshairs: false,
+					xDateFormat: '%d %B %Y',
+					shared: true
 				},
 
 				navigation: {
@@ -4505,7 +4523,7 @@ $(document).ready(function() {
 				},
 
 				series: seriesOptions,
-				colors: ['#64B5F6', '#9CCC65', 'red']
+				colors: ['#64B5F6', '#9CCC65']
 			});
 		}
 
